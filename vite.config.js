3414bb79bf4b6171d667b4d34a62f9b25c3bc321
alejandra <abarreto@360pmi.com>
2025-09-28 17:51:34 -0500

    deploy: GH Pages (base + docs + 404)

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
+  base: '/bloomery-site/',
   plugins: [react()],
-  build: { outDir: 'docs' }
+  build: { outDir: 'docs', emptyOutDir: true }
 })
